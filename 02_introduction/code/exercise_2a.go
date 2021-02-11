7ddaeed4aab62a9654477cf2fa4f1d16970af6df
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-02-11 11:20:45 -0300

    add solution exercise_2a.go

---
+++ 02_introduction/code/exercise_2a.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	/* part one */
+	fmt.Println("Hello world")
+
+	/* part two */
+	fmt.Printf(
+		"Hi! My name is %s. I have lived in %s for %d years. They say the weather is amazing, which is %t.\n", "Takayuki", "São Paulo", 23, false)
+}
