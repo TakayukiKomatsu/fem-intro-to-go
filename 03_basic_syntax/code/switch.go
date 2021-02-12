a4afcbe44dcdb67de8f1a6fc740c7c238ae0a729
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-02-12 12:54:48 -0300

    switch

--- 03_basic_syntax/code/switch.go
+++ 03_basic_syntax/code/switch.go
@@ -1,12 +1,10 @@
-/* Uncomment the entire file
- */
 package main
 
 import "fmt"
 
 func main() {
 
-	var city string
+	/* var city string
 
 	switch city {
 	case "Des Moines":
@@ -17,33 +15,32 @@ func main() {
 		fmt.Println("You live in Wisconsin")
 	default:
 		fmt.Println("You're not from around here.")
-	}
+	} */
 
 	// ****************************
-	var i int
-
-	switch {
-	case i > 10:
-		fmt.Println("Greater than 10")
-	case i < 10:
-		fmt.Println("Less than 10")
-	default:
-		fmt.Println("Is 10")
-	}
-
-	/* 	// ****************************
-	   	var i int = 9
-
+	/* 	var i int
 
 	   	switch {
-	   	case i != 10:
-	   		fmt.Println("Does not equal 10")
-	   		fallthrough
-	   	case i < 10:
-	   		fmt.Println("Less than 10")
 	   	case i > 10:
 	   		fmt.Println("Greater than 10")
+	   	case i < 10:
+	   		fmt.Println("Less than 10")
 	   	default:
 	   		fmt.Println("Is 10")
 	   	} */
+
+	// ****************************
+	var i int = 9
+
+	switch {
+	case i != 10:
+		fmt.Println("Does not equal 10")
+		fallthrough
+	case i < 10:
+		fmt.Println("Less than 10")
+	case i > 10:
+		fmt.Println("Greater than 10")
+	default:
+		fmt.Println("Is 10")
+	}
 }
