14732fb03a15aa044f7f9772e446a43e94cb2926
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-02-12 13:41:52 -0300

    functions

--- 04_complex_structures/code/functions.go
+++ 04_complex_structures/code/functions.go
@@ -1,15 +1,15 @@
 package main
 
-// import "fmt"
+import "fmt"
 
-// func printAge(age1, age2 int) (ageOfSally, ageOfBob int) {
-// 	ageOfSally = age1
-// 	ageOfBob = age2
-// 	return
-// }
+func printAge(ages ...int) {
+
+	for _, value := range ages {
+		fmt.Println(value)
+	}
+}
 
 func main() {
-	// 	x, y := printAge(10, 21)
-	// 	fmt.Println(x)
-	// 	fmt.Println(y)
+
+	printAge(16, 21, 30)
 }
